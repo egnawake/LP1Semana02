dd13c1c2b1d02de90cc7ff722d494645d58e2890
João Inácio <egnawake@gmail.com>
2022-03-03 12:51:44 +0000

    ValoresEspeciais: print min and max values

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"byte:\t{byte.MinValue} - {byte.MaxValue}");
+            Console.WriteLine($"sbyte:\t{sbyte.MinValue} - {sbyte.MaxValue}");
+            Console.WriteLine($"short:\t{short.MinValue} - {short.MaxValue}");
+            Console.WriteLine($"ushort:\t{ushort.MinValue} - {ushort.MaxValue}");
+            Console.WriteLine($"int:\t{int.MinValue} - {int.MaxValue}");
+            Console.WriteLine($"uint:\t{uint.MinValue} - {uint.MaxValue}");
+            Console.WriteLine($"long:\t{long.MinValue} - {long.MaxValue}");
+            Console.WriteLine($"ulong:\t{ulong.MinValue} - {ulong.MaxValue}");
+            Console.WriteLine($"float:\t{float.MinValue} - {float.MaxValue}");
+            Console.WriteLine($"double:\t{double.MinValue} - {double.MaxValue}");
+        }
+    }
+}
