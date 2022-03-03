24f775100dce75339027bb4334356653fb2f0bce
João Inácio <egnawake@gmail.com>
2022-03-03 12:14:18 +0000

    Cilindro: get height and radius from user

---
+++ Cilindro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float height;
+            float radius;
+            string inp;
+
+            Console.Write("Enter height: ");
+            inp = Console.ReadLine();
+            height = float.Parse(inp);
+
+            Console.Write("Enter radius: ");
+            inp = Console.ReadLine();
+            radius = float.Parse(inp);
+
+            Console.WriteLine($"Height = {height}");
+            Console.WriteLine($"Radius = {radius}");
+        }
+    }
+}
