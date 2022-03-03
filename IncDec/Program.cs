0f009b1e34bb484960ba3cfc544be73d09359fbe
João Inácio <egnawake@gmail.com>
2022-03-03 11:30:12 +0000

    Show result of expression with suffix ++

---
+++ IncDec/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 2;
+            
+            Console.WriteLine("Suffix");
+            Console.WriteLine($"a = {a}; b = {b}");
+            Console.WriteLine($"(a++) + b = {a++ + b}");
+            Console.WriteLine($"a = {a}; b = {b}");
+        }
+    }
+}
